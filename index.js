b37c9ad38f1207f4dc623e6fab3e289968e8ddc7
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2020-11-16 19:51:25 +0200

    remove scheme from url

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || 3200;
 
 var countries = [
-    { name: 'Common QA', url: 'https://hdot-public-qa.azurewebsites.net/', imageUrl: ''},
-    { name: 'Indonesia', url: 'https://tokobintang.co.id/', imageUrl: ''},
-    { name: 'Nigeria', url: 'https://quickdrinks.com/', imageUrl: ''}
+    { name: 'Common QA', url: 'hdot-public-qa.azurewebsites.net/', imageUrl: ''},
+    { name: 'Indonesia', url: 'tokobintang.co.id/', imageUrl: ''},
+    { name: 'Nigeria', url: 'quickdrinks.com/', imageUrl: ''}
 ]
 
 app.get('/api', (req, res, next) => {
