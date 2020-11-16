bc98ce6ccb9127812a310943795974a4eb69e9de
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2020-11-16 12:20:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3200;
+
+var countries = [
+    { name: 'Common QA', url: 'https://hdot-public-qa.azurewebsites.net/', imageUrl: ''},
+    { name: 'Indonesia', url: 'https://www.tokobintang.co.id/', imageUrl: ''},
+    { name: 'Nigeria', url: 'https://www.quickdrinks.com/', imageUrl: ''}
+]
+
+app.get('/api', (req, res, next) => {
+    res.send('Hello world');
+});
+
+app.get('/api/countries', function(req, res, next) {
+    res.send(countries);
+});
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+});
